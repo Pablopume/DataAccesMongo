8ddffb461dc4cbe3bd08a5a091d3f0554c65d5c0
Pablopume <pablo.serrano32@educa.madrid.org>
2024-02-12 23:11:20 +0100

    877

--- src/main/java/dao/imp/AggregationsImpl.java
+++ src/main/java/dao/imp/AggregationsImpl.java
@@ -5,7 +5,9 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Field;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 import dao.AggregationsDAO;
 import io.vavr.control.Either;
 import model.errors.OrderError;
@@ -15,9 +17,11 @@ import org.bson.types.ObjectId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Accumulators.*;
 import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.descending;
 
@@ -31,7 +35,7 @@ public class AggregationsImpl implements AggregationsDAO {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> menuItems = db.getCollection("menuitems");
             List<Document> document = menuItems.aggregate(Arrays.asList(
-                   sort(descending("price")), limit(1), project(fields(include("description"), excludeId())))).into(new ArrayList<>());
+                    sort(descending("price")), limit(1), project(fields(include("description"), excludeId())))).into(new ArrayList<>());
 
             String json = document.get(0).toJson();
             result = Either.right(json);
@@ -52,16 +56,16 @@ public class AggregationsImpl implements AggregationsDAO {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
             List<Document> document = customers.aggregate(Arrays.asList(
-                  match(Filters.eq("_id", new ObjectId("65c5f9337a122f76869ffdb3")))
-                    ,unwind("$orders")
-                    ,project(fields(include("first_name","orders.table_id"), excludeId())))).into(new ArrayList<>());
+                    match(eq("_id", new ObjectId("65c5f9337a122f76869ffdb3")))
+                    , unwind("$orders")
+                    , project(fields(include("first_name", "orders.table_id"), excludeId())))).into(new ArrayList<>());
 
             String json = document.get(0).toJson();
             result = Either.right(json);
 
 
         } catch (Exception e) {
-            result= Either.left(new OrderError(e.getMessage()));
+            result = Either.left(new OrderError(e.getMessage()));
         }
         return result;
     }
@@ -75,10 +79,10 @@ public class AggregationsImpl implements AggregationsDAO {
             MongoCollection<Document> customers = db.getCollection("customers");
             List<Document> document = customers.aggregate(Arrays.asList(
                     unwind("$orders")
-                    ,project(fields(include("orders.orderItemList")))
-                    ,unwind("$orders.orderItemList")
-                    ,group("$orders", sum("quantity", 1)),project(exclude("orderItemList.menuItemId")))).into(new ArrayList<>());
-            String json = document.get(0).toJson();
+                    , unwind("$orders.orderItemList")
+                    , group("$orders.date", sum("quantity", "$orders.orderItemList.quantity")), project(excludeId()))).into(new ArrayList<>());
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
+
             result = Either.right(json);
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
@@ -92,13 +96,22 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            List<Document> document = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    unwind("$orders.orderItemList"),
+                    match(eq("orders.orderItemList.menuItemId", 3)),
+                    project(include("first_name"))
+            )).into(new ArrayList<>());
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
 
-            ;
+            result = Either.right(json);
+
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
+
 
     @Override
     public Either<OrderError, String> e() {
@@ -106,12 +119,15 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            List<Document> document = customers.aggregate(Arrays.asList(unwind("$orders"), unwind("$orders.orderItemList"), group("$orders.date", avg("average", "$orders.orderItemList.quantity")), project(excludeId()))).into(new ArrayList<>());
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
 
-            ;
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
+
         }
-        return null;
+        return result;
     }
 
     @Override
@@ -120,12 +136,21 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            //get the menuItemId most requested
+            List<Document> document = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    unwind("$orders.orderItemList"),
+                    group("$orders.orderItemList.menuItemId", sum("total", "$orders.orderItemList.quantity")),
+                    sort(descending("total")),
+                    limit(1)
+            )).into(new ArrayList<>());
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
 
-            ;
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
 
     @Override
@@ -134,12 +159,20 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+//Show a list with the number of items of each type requested by a selected customer
+            List<Document> document = customers.aggregate(Arrays.asList(
+                    match(eq("_id", new ObjectId("65c5f9337a122f76869ffdb3"))
+                    ), unwind("$orders"), unwind("$orders.orderItemList")
+                    , group("$orders.orderItemList.menuItemId"
+                            , sum("total", "$orders.orderItemList.quantity"))
+                    , project(excludeId()))).into(new ArrayList<>());
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
 
-            ;
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
 
     @Override
@@ -148,12 +181,15 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            List<Document> document = customers.aggregate(Arrays.asList(
+                    unwind("$orders"), group("$orders.table_id", sum("table", 0)), sort(Sorts.descending("table")), project(exclude("table")), limit(1))).into(new ArrayList<>());
 
-            ;
+            String json = document.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
 
     @Override
@@ -163,12 +199,25 @@ public class AggregationsImpl implements AggregationsDAO {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
 
-            ;
+            List<Document> documents = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    group(
+                            Filters.and(Filters.eq("_id", "$_id"), Filters.eq("table_id", "$orders.table_id")),
+                            sum("total", 1)
+                    ),
+                    sort(descending("total")),
+                    group("$_id._id", first("most_requested_table", "$_id.table_id")),
+                    project(fields(include("most_requested_table")))
+            )).into(new ArrayList<>());
+
+            String json = documents.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
+
 
     @Override
     public Either<OrderError, String> j() {
@@ -176,13 +225,27 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            List<Document> documents = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    unwind("$orders.orderItemList"),
+                    group(
+                            Filters.and(Filters.eq("date", "$orders.date"), Filters.eq("menuItemId", "$orders.orderItemList.menuItemId")),
+                            sum("menuItem", "$orders.orderItemList.quantity")
+                    ),
+                    match(eq("menuItem", 1)),
+                    addFields(new Field<>("menuItem", "$_id.menuItemId"),new Field<>("date", "$_id.date")),
+                    project(excludeId())
+            )).into(new ArrayList<>());
 
-            ;
+            String json = documents.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
+
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
+
 
     @Override
     public Either<OrderError, String> k() {
@@ -190,39 +253,85 @@ public class AggregationsImpl implements AggregationsDAO {
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
+            //Price paid for each order (Use $lookup)
 
-            ;
+
+            List<Document> documents = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    lookup("menuitems", "orders.orderItemList.menuItemId", "id", "menuItems"),
+                    unwind("$menuItems"),
+                    group("$orders._id", sum("total", "$menuItems.price")),
+                    project(fields(excludeId(), include("total"))
+                    )
+            )).into(new ArrayList<>());
+
+
+            String json = documents.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
+
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
 
     @Override
     public Either<OrderError, String> l() {
+        //l. Get the name of the customer that has spent more money in the restaurant
         Either<OrderError, String> result;
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
 
-            ;
+            List<Document> documents = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    lookup("menuitems", "orders.orderItemList.menuItemId", "id", "menuItems"),
+                    unwind("$menuItems"),
+                    group("$orders.customer_id", sum("total", "$menuItems.price")),
+                    sort(descending("total")),
+                    limit(1),
+                    lookup("customers", "_id", "customer_id", "customer"),
+                    unwind("$customer"),
+                    project(fields(include("first_name"), excludeId())
+                    )
+            )).into(new ArrayList<>());
+
+            String json = documents.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
 
     @Override
     public Either<OrderError, String> m() {
         Either<OrderError, String> result;
+        //total amount
         try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
             MongoDatabase db = mongo.getDatabase("PabloSerrano_Restaurant");
             MongoCollection<Document> customers = db.getCollection("customers");
 
-            ;
+            List<Document> documents = customers.aggregate(Arrays.asList(
+                    unwind("$orders"),
+                    unwind("$orders.orderItemList"), // Unwind orderItemList
+                    lookup("menuitems", "orders.orderItemList.menuItemId", "id", "menuItems"),
+                    unwind("$menuItems"),
+                    group("$orders.customer_id",
+                            sum("total", new Document("$multiply", Arrays.asList("$menuItems.price", "$orders.orderItemList.quantity")))), // Multiply price by quantity
+                    project(fields(excludeId(), include("total")))
+            )).into(new ArrayList<>());
+
+            String json = documents.stream().map(Document::toJson).collect(Collectors.joining(","));
+            result = Either.right(json);
+
         } catch (Exception e) {
             result = Either.left(new OrderError(e.getMessage()));
         }
-        return null;
+        return result;
     }
+
+
+
+
 }
